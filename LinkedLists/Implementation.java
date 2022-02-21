5be2ce1ea69675a70dc33fb2d6931b0e3892ee21
PratapTyagi <tyagipratap111@gmail.com>
2022-02-21 12:18:18 +0530

    Implementation of linked list

---
+++ LinkedLists/Implementation.java
@@ -0,0 +1,57 @@
+package LinkedLists;
+
+import java.io.*;
+
+class Implementation {
+
+    Node head;
+
+    class Node {
+        Node next;
+        int data;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    void push(int d) {
+        if (head == null) {
+            head = new Node(d);
+            return;
+        }
+
+        Node node = new Node(d);
+        Node temp = head;
+        node.next = null;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+
+    void printList() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Implementation list = new Implementation();
+
+        System.out.println("Enter items in list: ");
+        String[] d = br.readLine().split(" ");
+
+        for (int i = 0; i < d.length; i++)
+            list.push(Integer.parseInt(d[i]));
+
+        list.printList();
+    }
+}
