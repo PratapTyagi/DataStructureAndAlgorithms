bdae28cc45255d33a4814849c3b140f85449ec16
PratapTyagi <tyagipratap111@gmail.com>
2022-03-01 10:30:27 +0530

    Using split

---
+++ Strings/ReverseWords.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        String s = "i.like.this.program.very.much";
+
+        String[] str = s.split("\\.");
+        String string = "";
+        for (int i = str.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                string += str[i];
+                break;
+            }
+            string += str[i] + ".";
+        }
+
+        System.out.println(string);
+    }
+}
