56852d5f35bcc7785d0707db0e64b3e530d8875b
PratapTyagi <tyagipratap111@gmail.com>
2022-03-01 10:40:34 +0530

    O(N*max(|arri|)) time and O(max(|arri|)) space

---
+++ Strings/LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+package Strings;
+
+public class LongestCommonPrefix {
+    static String longestCommonPrefix(String arr[], int n) {
+        String s = "";
+        int i = 0;
+        while (i < arr[0].length()) {
+            char c = arr[0].charAt(i);
+            for (int j = 1; j < arr.length; j++) {
+                if (s.length() == 0 && arr[j].length() > i && c != arr[j].charAt(i))
+                    return "-1";
+                if (arr[j].length() > i && c != arr[j].charAt(i))
+                    return s;
+
+                if (arr[j].length() <= i)
+                    return s;
+            }
+            s += c;
+            i++;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        String arr[] = new String[] { "geeksforgeeks", "geeks", "geek",
+                "geezer" };
+
+        System.out.println(longestCommonPrefix(arr, arr.length));
+    }
+}
