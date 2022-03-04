e4427b7eea8a1b01e7117e1a3ae1cac40f22d611
PratapTyagi <tyagipratap111@gmail.com>
2022-03-04 22:45:47 +0530

    top down method

---
+++ DynamicProgramming/Knapsack01/EqualSumPartitionTopDown.java
@@ -0,0 +1,39 @@
+package DynamicProgramming.Knapsack01;
+
+public class EqualSumPartitionTopDown {
+    static boolean isPossible(int[] arr, int sum, int n) {
+        int[][] dp = new int[n + 1][sum + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (i == 0)
+                    dp[i][j] = 0;
+                if (j == 0)
+                    dp[i][j] = 1;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (arr[i - 1] <= j)
+                    dp[i][j] = (dp[i - 1][j] != 0 || dp[i - 1][j - arr[i - 1]] != 0) ? 1 : 0;
+                else
+                    dp[i][j] = 0;
+            }
+        }
+        return dp[n][sum] == 1 ? true : false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 5, 3, 8, 0 };
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++)
+            sum += arr[i];
+
+        if (sum % 2 != 0) {
+            System.out.println(false);
+            return;
+        }
+        boolean res = isPossible(arr, sum / 2, arr.length);
+        System.out.println(res);
+    }
+}
