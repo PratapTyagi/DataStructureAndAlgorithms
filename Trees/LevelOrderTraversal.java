9de36204d6180e0d2942a0031cce2c6f0272c75a
PratapTyagi <tyagipratap111@gmail.com>
2022-03-26 13:44:21 +0530

    Level order using queue

---
+++ Trees/LevelOrderTraversal.java
@@ -0,0 +1,38 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    static ArrayList<Integer> levelOrder(Node node) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        Queue<Node> queue = new ArrayDeque<>();
+
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            // rpa -> remove, process, add
+            for (int i = 0; i < queue.size(); i++) {
+                Node peek = queue.poll();
+
+                al.add(peek.data);
+                if (peek.left != null)
+                    queue.add(peek.left);
+                if (peek.right != null)
+                    queue.add(peek.right);
+            }
+        }
+
+        return al;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = { 1, 2, 4, null, null, 5, null, null, 3, null, null };
+
+        Node root = new Node(arr[0]);
+        Implementation i = new Implementation();
+        i.createBinaryTree(arr, root);
+        ArrayList<Integer> al = levelOrder(root);
+
+        for (Integer ele : al)
+            System.out.print(ele + " ");
+    }
+}
