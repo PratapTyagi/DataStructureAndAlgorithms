8b14f28d55903d60d0323410917277cebe408b52
PratapTyagi <tyagipratap111@gmail.com>
2022-03-30 12:23:28 +0530

    using recursion

---
+++ Trees/ConstructTreeFromPreorderTraversal.java
@@ -0,0 +1,34 @@
+public class ConstructTreeFromPreorderTraversal {
+    static Node create(int n, int[] pre, char[] preLN, int[] preStartIndex) {
+        if (preStartIndex[0] >= n)
+            return null;
+
+        Node root = new Node(pre[preStartIndex[0]]);
+
+        if (preLN[preStartIndex[0]] == 'N') {
+            preStartIndex[0]++;
+            root.left = create(n, pre, preLN, preStartIndex);
+            root.right = create(n, pre, preLN, preStartIndex);
+        } else {
+            preStartIndex[0]++;
+            return root;
+        }
+
+        return root;
+    }
+
+    static Node constructTree(int n, int pre[], char preLN[]) {
+        int preStartIndex[] = new int[] { 0 };
+        return create(n, pre, preLN, preStartIndex);
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        int pre[] = { 10, 30, 20, 5, 15 };
+        char preLN[] = { 'N', 'N', 'L', 'L', 'L' };
+
+        Implementation i = new Implementation();
+        Node root = constructTree(N, pre, preLN);
+        i.inOrder(root);
+    }
+}
