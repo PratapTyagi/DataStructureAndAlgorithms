d1261402fd85a3ad4384b0783cb1d30ca5fc480c
PratapTyagi <tyagipratap111@gmail.com>
2022-03-30 15:29:46 +0530

    lca recursion

---
+++ Trees/LCABinaryTree.java
@@ -0,0 +1,31 @@
+public class LCABinaryTree {
+    static Node lca(Node root, int n1, int n2) {
+        if (root == null)
+            return null;
+
+        if (root.data == n1 || root.data == n2)
+            return root;
+
+        Node left = lca(root.left, n1, n2);
+        Node right = lca(root.right, n1, n2);
+
+        if (left != null && right != null)
+            return root;
+
+        return left != null ? left : right;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = { 1, 2, 3, null, null, null, null };
+        Node root = new Node(arr[0]);
+
+        Implementation i = new Implementation();
+        // We got our binary tree
+        i.createBinaryTree(arr, root);
+        // Traverse
+        i.inOrder(root);
+
+        Node lcaNode = lca(root, 1, 3);
+        System.out.println("\n" + lcaNode.data);
+    }
+}
