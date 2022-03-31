0b21f237a333cdc63b608f5bfbc2b39db91dffc0
PratapTyagi <tyagipratap111@gmail.com>
2022-03-31 10:22:11 +0530

    max path from leaf to root

---
+++ Trees/MaximumSumLeafToRootPath.java
@@ -0,0 +1,23 @@
+public class MaximumSumLeafToRootPath {
+    static int max(Node root) {
+        if (root == null)
+            return 0;
+
+        return Math.max(max(root.left), max(root.right)) + root.data;
+    }
+
+    static int maxPathSum(Node root) {
+        if (root == null)
+            return 0;
+
+        return max(root);
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = { 1, 2, null, null, 3, null, null };
+        Node root = new Node(arr[0]);
+        Implementation i = new Implementation();
+        i.createBinaryTree(arr, root);
+        System.out.println(maxPathSum(root));
+    }
+}
