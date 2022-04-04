01e252989f9569df0e88a907692c4d48c866a26f
PratapTyagi <tyagipratap111@gmail.com>
2022-04-04 11:40:18 +0530

    reversing and adding 1 to list

---
+++ LinkedLists/Add1ToNumber.java
@@ -0,0 +1,48 @@
+public class Add1ToNumber {
+    static LinkedListNode reverse(LinkedListNode root) {
+        LinkedListNode prev = null, curr = root, next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static LinkedListNode addOne(LinkedListNode root) {
+        LinkedListNode head = reverse(root);
+
+        LinkedListNode temp = head;
+        int carry = 1;
+        LinkedListNode prev = null;
+
+        while (temp != null) {
+            int data = temp.data + carry;
+            temp.data = data % 10;
+            carry = data / 10;
+            prev = temp.next == null ? temp : null;
+            temp = temp.next;
+        }
+
+        if (carry != 0)
+            prev.next = new LinkedListNode(carry);
+
+        return reverse(head);
+    }
+
+    public static void main(String[] args) {
+        LinkedListImplementation i = new LinkedListImplementation();
+
+        int[] arr = new int[] { 0, 5, 6, 8, 9 };
+        for (int j = 0; j < arr.length; j++)
+            i.push(arr[j]);
+
+        LinkedListNode head = i.head;
+        LinkedListNode root = addOne(head);
+
+        i.printList(root);
+    }
+}
