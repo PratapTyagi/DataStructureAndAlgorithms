1774c41c35b4b56258c320be3d259fca0e3a0362
PratapTyagi <tyagipratap111@gmail.com>
2022-04-18 09:55:16 +0530

    minimum number of operations required to convert s to t

---
+++ DynamicProgramming/Randoms/EditDistance.java
@@ -0,0 +1,25 @@
+public class EditDistance {
+    static int editDistance(String s, String t) {
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0)
+                    dp[i][j] = j;
+                else if (j == 0)
+                    dp[i][j] = i;
+                else if (s.charAt(i - 1) == t.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s = "geek", t = "gesek";
+        System.out.println(editDistance(s, t));
+    }
+}
