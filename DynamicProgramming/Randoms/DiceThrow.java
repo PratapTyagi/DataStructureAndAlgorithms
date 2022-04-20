f1e879daf451d04404c46d4668a82dd8d6d89bc5
PratapTyagi <tyagipratap111@gmail.com>
2022-04-20 11:58:28 +0530

    dp bottom up

--- DynamicProgramming/Randoms/DiceThrow.java
+++ DynamicProgramming/Randoms/DiceThrow.java
@@ -18,8 +18,18 @@ class DiceThrow {
     }
 
     static long noOfWays(int m, int n, int x) {
-        HashMap<String, Long> map = new HashMap<>();
-        return solve(m, n, x, map);
+        // HashMap<String, Long> map = new HashMap<>();
+        // return solve(m, n, x, map);
+        long[][] dp = new long[n + 1][x + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= x; j++)
+                for (int k = 1; k <= m && k <= j; k++)
+                    dp[i][j] += dp[i - 1][j - k];
+        }
+
+        return dp[n][x];
     }
 
     public static void main(String[] args) {
