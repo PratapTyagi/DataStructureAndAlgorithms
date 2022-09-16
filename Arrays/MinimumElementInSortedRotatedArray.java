e421479f08ae2984f03c0388662e7a1733b9a1a2
PratapTyagi <tyagipratap111@gmail.com>
2022-09-16 09:21:45 +0530

    using the property, where two sets of sorted array intersect

--- Arrays/MinimumElementInSortedRotatedArray.java
+++ Arrays/MinimumElementInSortedRotatedArray.java
@@ -1,22 +1,19 @@
 public class MinimumElementInSortedRotatedArray {
     static int findMin(int arr[], int n) {
         int start = 0, end = n - 1;
+        if (arr[start] <= arr[end])
+            return arr[0];
+
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (mid == 0)
-                return Math.min(arr[end], arr[start]);
-            else if (mid == n - 1)
+            if (arr[mid] > arr[mid + 1])
+                return arr[mid + 1];
+            else if (arr[mid] < arr[mid - 1])
                 return arr[mid];
-
-            int next = (mid + 1) % n;
-            int prev = (mid + n - 1) % n;
-
-            if (arr[mid] < arr[next] && arr[mid] < arr[prev])
-                return arr[mid];
-            else if (arr[mid] > arr[end])
+            else if (arr[start] <= arr[mid])
                 start = mid + 1;
-            else
+            else if (arr[mid] <= arr[end])
                 end = mid - 1;
         }
         return -1;
