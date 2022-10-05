d5003df8d85d510449a7b636fd48528f5755ccdb
PratapTyagi <tyagipratap111@gmail.com>
2022-10-05 22:12:18 +0530

    few efficient approches

--- BinarySearchTree/FindPairWithTarget.java
+++ BinarySearchTree/FindPairWithTarget.java
@@ -1,4 +1,39 @@
+import java.util.Stack;
+
 public class FindPairWithTarget {
+    private static class BSTIterator {
+        Stack<BSTNode> stack = new Stack<>();
+        boolean isReversed = false;
+
+        public BSTIterator(BSTNode root, boolean isReversed) {
+            pushAll(root);
+            this.isReversed = isReversed;
+        }
+
+        private void pushAll(BSTNode root) {
+            while (root != null) {
+                stack.push(root);
+                if (!isReversed)
+                    root = root.left;
+                else
+                    root = root.right;
+            }
+        }
+
+        public int next() {
+            BSTNode tempNode = stack.pop();
+            if (isReversed)
+                pushAll(tempNode.left);
+            else
+                pushAll(tempNode.right);
+            return tempNode.data;
+        }
+
+        // public boolean hasNext() {
+        // return !stack.isEmpty();
+        // }
+    }
+
     static boolean find(BSTNode root, int complement) {
         if (root == null)
             return false;
@@ -26,7 +61,29 @@ public class FindPairWithTarget {
     }
 
     static int isPairPresent(BSTNode root, int target) {
-        return isPresent(root, root, target);
+        // return isPresent(root, root, target); // O(N^2) time, O(1) space complexity
+
+        // Approch 2 O(N) time and space complexity
+        // Store in order in array and then use 2sum approch to find target presence
+
+        // Approch 3
+        // BST Iterator Time ≈ O(1) and space O(ht)
+
+        BSTIterator l = new BSTIterator(root, false);
+        BSTIterator r = new BSTIterator(root, true);
+
+        int i = l.next();
+        int j = r.next();
+        while (i < j) {
+            if (i + j == target)
+                return 1;
+            if (i + j < target)
+                i = l.next();
+            else
+                j = r.next();
+        }
+
+        return 0;
     }
 
     public static void main(String[] args) {
