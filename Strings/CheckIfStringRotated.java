fcad3c76b32031bdd261745d49423586057f227a
PratapTyagi <tyagipratap111@gmail.com>
2022-10-13 10:59:05 +0530

    new method

--- Strings/CheckIfStringRotated.java
+++ Strings/CheckIfStringRotated.java
@@ -1,20 +1,6 @@
 public class CheckIfStringRotated {
     static boolean isRotated(String a, String b) {
-        if (a.equals(b)) {
-            return true;
-        }
-        String start = b.substring(0, 2);
-        String end = b.substring(b.length() - 2);
-        String mid = b.substring(2, b.length() - 2);
-        b = end + start + mid;
-        if (a.equals(b)) {
-            return true;
-        }
-        b = end + mid + start;
-        if (a.equals(b)) {
-            return true;
-        }
-        return false;
+        return (a.length() == b.length()) && ((a + a).indexOf(b) != -1);
     }
 
     public static void main(String[] args) {
