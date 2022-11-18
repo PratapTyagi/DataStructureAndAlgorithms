668f26506391f7823707c005de9c7620fb50186b
PratapTyagi <tyagipratap111@gmail.com>
2022-11-18 21:49:32 +0530

    using bits logic

---
+++ BitManipulation/DivideTwoIntegers.java
@@ -0,0 +1,8 @@
+public class DivideTwoIntegers {
+    // 7n/8
+    public static void main(String[] args) {
+        int n = 43;
+        int val = ((n << 3) - n) >> 3;
+        System.out.println(val);
+    }
+}
